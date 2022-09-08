d989175c1015d6e1305eaf36804708e176dffac1
Chinouo <574303954@qq>
2022-09-08 19:20:07 +0800

    add glm hash header

--- engine/core/math.hpp
+++ engine/core/math.hpp
@@ -9,7 +9,9 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_SSE2
+#include "third_party/glm/glm/ext.hpp"
 #include "third_party/glm/glm/glm.hpp"
+#include "third_party/glm/glm/gtx/hash.hpp"
 
 namespace ShaderStory {
 
@@ -18,6 +20,10 @@ using namespace glm;
 constexpr vec3 X_AXIS = {1.f, 0.f, 0.f};
 constexpr vec3 Y_AXIS = {0.f, 1.f, 0.f};
 constexpr vec3 Z_AXIS = {0.f, 0.f, 1.f};
+
+constexpr vec3 world_up = Z_AXIS;
+constexpr vec3 world_front = Y_AXIS;
+constexpr vec3 world_right = X_AXIS;
 
 }  // namespace ShaderStory
 
