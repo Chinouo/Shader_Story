d49799cabfb8504249a92d97821798bf994a2cc5
Chinouo <574303954@qq>
2022-09-08 19:23:20 +0800

    add oop callback

--- engine/runtime/window/window_system.cpp
+++ engine/runtime/window/window_system.cpp
@@ -2,6 +2,20 @@
 
 namespace ShaderStory {
 
+void WindowSystem::keyCallback(GLFWwindow* window, int key, int scancode,
+                               int action, int mods) {
+  WindowSystem* wd_sys =
+      static_cast<WindowSystem*>(glfwGetWindowUserPointer(window));
+  if (wd_sys) wd_sys->onKey(key, scancode, action, mods);
+}
+
+void WindowSystem::cursorPosCallback(GLFWwindow* window, double xpos,
+                                     double ypos) {
+  WindowSystem* wd_sys =
+      static_cast<WindowSystem*>(glfwGetWindowUserPointer(window));
+  if (wd_sys) wd_sys->onCursorPos(xpos, ypos);
+}
+
 WindowSystem::WindowSystem(int width, int height)
     : m_width(width), m_height(height) {}
 
@@ -13,19 +27,56 @@ void WindowSystem::Initialize() {
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
   glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-  wd_ = glfwCreateWindow(m_width, m_height, "ShaderStory", nullptr, nullptr);
-  ASSERT(wd_ && "Falied to create GLFW Window.");
+  m_wd = glfwCreateWindow(m_width, m_height, "ShaderStory", nullptr, nullptr);
+  ASSERT(m_wd && "Falied to create GLFW Window.");
+
+  InstallCallbacks();
 }
 
 void WindowSystem::Destory() {
-  glfwDestroyWindow(wd_);
+  glfwDestroyWindow(m_wd);
   glfwTerminate();
 }
 
 bool WindowSystem::ShouldCloseWindow() const {
-  return glfwWindowShouldClose(wd_);
+  return glfwWindowShouldClose(m_wd);
 }
 
-GLFWwindow* WindowSystem::GetWindow() const { return wd_; }
+GLFWwindow* WindowSystem::GetWindow() const { return m_wd; }
+
+void WindowSystem::InstallCallbacks() {
+  glfwSetWindowUserPointer(m_wd, this);
+  glfwSetCursorPosCallback(m_wd, WindowSystem::cursorPosCallback);
+  glfwSetKeyCallback(m_wd, WindowSystem::keyCallback);
+}
+
+void WindowSystem::UninstallCallbacks() {
+  glfwSetCursorPosCallback(m_wd, nullptr);
+  glfwSetKeyCallback(m_wd, nullptr);
+}
+
+void WindowSystem::InstallImGuiCallbacks() {}
+
+void WindowSystem::UninstallImGuiCallbacks() {}
+
+void WindowSystem::registerKeyCallback(KeyCallback&& callback) {
+  m_key_callbacks.emplace_back(callback);
+}
+
+void WindowSystem::registerCurPosCallbakc(CursorPosCallback&& callback) {
+  m_cur_callbacks.emplace_back(callback);
+}
+
+void WindowSystem::onKey(int key, int scancode, int action, int mods) {
+  for (const auto& callback : m_key_callbacks) {
+    callback(key, scancode, action, mods);
+  }
+}
+
+void WindowSystem::onCursorPos(double xpos, double ypos) {
+  for (const auto& callback : m_cur_callbacks) {
+    callback(xpos, ypos);
+  }
+}
 
 }  // namespace ShaderStory
--- engine/runtime/window/window_system.hpp
+++ engine/runtime/window/window_system.hpp
@@ -1,6 +1,9 @@
 #ifndef WINDOW_SYSTEM_HPP
 #define WINDOW_SYSTEM_HPP
 
+#include <functional>
+#include <vector>
+
 #include "engine/common/macros.h"
 
 #ifndef GLFW_INCLUDE_VULKAN
@@ -9,29 +12,53 @@
 #endif
 
 namespace ShaderStory {
+using KeyCallback = std::function<void(int, int, int, int)>;
+using CursorPosCallback = std::function<void(int, int)>;
 
-class WindowSystem {
+class WindowSystem final {
  public:
   WindowSystem(int width, int height);
-
   ~WindowSystem();
 
   void Initialize();
-
   void Destory();
+
+  // on game play mode
+  void InstallCallbacks();
+  void UninstallCallbacks();
+
+  // on imgui mode
+  void InstallImGuiCallbacks();
+  void UninstallImGuiCallbacks();
 
   /// only used by Vulkan RHI surface create.
   GLFWwindow* GetWindow() const;
 
   bool ShouldCloseWindow() const;
 
+  // we prefer r-ref, persist callback.
+  void registerKeyCallback(KeyCallback&&);
+  void registerCurPosCallbakc(CursorPosCallback&&);
+
  private:
-  GLFWwindow* wd_;
+  GLFWwindow* m_wd;
 
   int m_width;
   int m_height;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(WindowSystem);
+
+  // internal callback.
+  void onKey(int key, int scancode, int action, int mods);
+  void onCursorPos(double xpos, double ypos);
+
+  std::vector<KeyCallback> m_key_callbacks;
+  std::vector<CursorPosCallback> m_cur_callbacks;
+
+  static void keyCallback(GLFWwindow* window, int key, int scancode, int action,
+                          int mods);
+
+  static void cursorPosCallback(GLFWwindow* window, double xpos, double ypos);
 };
 
 }  // namespace ShaderStory
